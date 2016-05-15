0c6fb7e3d687f8181af3cf0fbdbf6592cd0c9e3a
F74041056 <decade910831@gmail.com>
2016-05-15 11:52:06 +0800

    Add Game.cpp to pd2-Taiko

---
+++ Game.cpp
@@ -0,0 +1,57 @@
+#include "Game.h"
+#include <QImage>
+#include <QGraphicsTextItem>
+//#include <QTimer>
+//#include <QFont>
+#include <QMediaPlayer>
+
+
+
+
+Game::Game(QWidget *parent)
+{
+    //create the scene
+    scene = new QGraphicsScene();
+    scene->setSceneRect(0,0,800,600);  //make the scene 800x490
+    setBackgroundBrush(QBrush(QImage(":/images/bg.png")));  //set background picture
+
+    //make the newly created scene the scene to visualize
+    setScene(scene);
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setFixedSize(800,600);
+
+
+    //create the player
+    player = new Player();
+    player->setPos(700,130);
+    scene->addItem(player);
+    //make the player focusable and set it to be the current focus
+//    player->setFlag(QGraphicsItem::ItemIsFocusable);
+//    player->setFocus();
+
+
+    //create the score
+    score = new Score();
+    scene->addItem(score);
+
+    time = new Time();
+    time->setPos(time->x()+600,time->y());
+    scene->addItem(time);
+
+
+    miss = new Miss();
+    miss->setPos(miss->x()+250,miss->y());
+    scene->addItem(miss);
+
+
+    //play background music
+//    QMediaPlayer *music = new QMediaPlayer();
+//    music->setMedia(QUrl("qrc:/sounds/bgsound.mp3"));
+//    music->play();
+
+
+    show();
+}
+
+
